3eb8556c4a0041e577aa944518ea2403ab4e6055
Your Name <you@example.com>
2019-12-09 13:04:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
+
+const initialActors = {
+  listName: 'Best',
+  actors: [
+    'Bogusław Linda', 'Tom Hanks', 'Arnold Schwarzeneger'
+  ]
+}
+const initialMovies = {
+  listName: 'Favourite',
+  movies: [
+    'Rambo III', 'Kiler', 'Terminator'
+  ]
+}
+function movies(state = initialMovies, action) {
+  switch (action.type) {
+    case 'RESET_MOVIE':
+      return{
+        ...state, movies: []
+      }
+    case 'ADD_MOVIE':
+      return{
+        ...state, movies: [...state.movies, action.film]
+      }
+    case 'DELETE_MOVIE':
+      return{
+        ...state, movies: [...state.movies.filter(el => el !== action.film)]
+      }
+       
+    default:
+      return state;
+  }
+}
+function actors(state = initialActors, action) {
+  switch (action.type) {
+    case 'RESET_ACTOR':
+      return{
+        ...state, actors: []
+      }
+    case 'ADD_ACTOR':
+      return{
+        ...state, actors: [...state.actors, action.actor]
+      }
+    case 'DELETE_ACTOR':
+      return{
+        ...state, actors: [...state.actors.filter(el => el !== action.actor)]
+      }
+       
+    default:
+      return state;
+  }
+}
+
+const rootReducer = combineReducers({ movies, actors })
+const store = createStore(rootReducer, composeWithDevTools())
+
+window.store = store;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello redux</h1>
     </div>
-  );
+  )
 }
 
 export default App;
